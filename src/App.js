b952e0ac5ca70418dfba60a6525dcae4489d2542
adamfrly <adamfrly@gmail.com>
2022-05-06 16:53:17 -0400

    added hash routing capability

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Routes, Link } from "react-router-dom";
+import PastProjects from './PastProjects';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Adam's Website
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <nav>
+            <ul id="navigation">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/projects">Projects</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/projects" element={<PastProjects />}/>
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/PastProjects.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const divStyle = {
+    color:'black'
+};
+
+class PastProjects extends React.Component {
+    
+    render() {
+        return (
+            <div style={divStyle}>
+                <h2>Past Projects Page</h2>
+                <main>
+                    <p>This section contains information about the past projects I've worked on. Coming soon...</p>
+                </main>
+            </div>
+        )
+    }
+}
+
+
+
+export default PastProjects;
